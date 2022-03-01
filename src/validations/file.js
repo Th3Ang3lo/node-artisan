579c3d20d088301b257d69cafb2b5b4a0495a259
Angelo Soares <angeelosoares@gmail.com>
2022-03-01 01:08:00 -0300

    refactor: verify if file name start with number

--- src/validations/file.js
+++ src/validations/file.js
@@ -1,11 +1,16 @@
 import fs from 'fs'
-import path from 'path'
 
 import { red } from '../helpers/colors.js'
+
+import { stringStartsWithNumber } from '../helpers/string.js'
 
 export const validateFileName = (filePath, fileName, fileType) => {
   if(!fileName) {
     throw red(`OPS! Please, inform your ${fileType} name.`)
+  }
+
+  if (stringStartsWithNumber(fileName)) {
+    throw red('OPS! The file starts with number.')
   }
 
   if (fs.existsSync(filePath)) {
